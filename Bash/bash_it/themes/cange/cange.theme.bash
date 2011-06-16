509309638aa99de4adc49bce0deb884d9e6c8544
Christian Angermann <angermann@ubilabs.net>
2011-06-16 10:27:41 +0200

    Update bash prompt theme

--- Bash/bash_it/themes/cange/cange.theme.bash
+++ Bash/bash_it/themes/cange/cange.theme.bash
@@ -2,24 +2,26 @@
 
 # cange theme by Christian Angermann <http://github.com/cange>
 git_prompt_status() {
+  # if [ -n "$(git rev-parse --git-dir | grep 'git' 2> /dev/null)" ]; then
   if git rev-parse --git-dir >/dev/null 2>&1; then
-    if [ -n "$(git status | grep 'Changes not staged' 2> /dev/null)" ]; then
-      color="${background_red} ✗$(git_prompt_info)"
+    if [ -n "$(git status | grep 'Changed but not updated' 2> /dev/null)" ]; then
+    #if [ -n "$(git status | grep 'Changes not staged' 2> /dev/null)" ]; then
+      color="${red} ✗$(git_prompt_info)"
     elif [ -n "$(git status | grep 'Changes to be committed' 2> /dev/null)" ]; then
-      color="${background_yellow} ^$(git_prompt_info)"
+      color="${yellow} ^$(git_prompt_info)"
     elif [ -n "$(git status | grep 'Untracked files' 2> /dev/null)" ]; then
-      color="${background_purple} +$(git_prompt_info)"
+      color="${purple} +$(git_prompt_info)"
     elif [ -n "$(git status | grep 'nothing to commit' 2> /dev/null)" ]; then
-      color="${background_green} ✓$(git_prompt_info)"
+      color="${green} ✓$(git_prompt_info)"
     else
-      color=""
+      color="$(git_prompt_info)"
     fi
   fi
   echo "$color${normal}"
 }
 # for example:
 # ✓ master cange workspace/ $
-PROMPT="\[\$(git_prompt_status)\]${bold_white}\u ${bold_cyan}\W ${normal}\$ "
+PROMPT="\[\$(git_prompt_status)\]${cyan}\W ${normal}\$ "
 ## git-theme
 # feel free to change git chars.
 GIT_THEME_PROMPT_DIRTY=""
@@ -32,3 +34,4 @@ SCM_THEME_PROMPT_DIRTY=""
 SCM_THEME_PROMPT_CLEAN=""
 SCM_THEME_PROMPT_PREFIX=""
 SCM_THEME_PROMPT_SUFFIX=""
+
