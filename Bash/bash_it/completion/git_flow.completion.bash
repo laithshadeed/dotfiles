16e3fbbb7cf450aef0b3c7dd783d9cfdd4de4c9b
Christian Angermann <angermann@ubilabs.net>
2011-06-21 15:06:26 +0200

    Update git flow completion

--- Bash/bash_it/completion/git_flow.completion.bash
+++ Bash/bash_it/completion/git_flow.completion.bash
@@ -2,59 +2,63 @@
 #
 # git-flow-completion
 # ===================
-# 
+#
 # Bash completion support for [git-flow](http://github.com/nvie/gitflow)
-# 
+#
 # The contained completion routines provide support for completing:
-# 
+#
 #  * git-flow init and version
 #  * feature, hotfix and release branches
-#  * remote feature branch names (for `git-flow feature track`)
-# 
-# 
+#  * remote feature, hotfix and release branch names
+#
+#
 # Installation
 # ------------
-# 
+#
 # To achieve git-flow completion nirvana:
-# 
+#
 #  0. Install git-completion.
-# 
+#
 #  1. Install this file. Either:
-# 
+#
 #     a. Place it in a `bash-completion.d` folder:
-# 
+#
 #        * /etc/bash-completion.d
 #        * /usr/local/etc/bash-completion.d
 #        * ~/bash-completion.d
-# 
+#
 #     b. Or, copy it somewhere (e.g. ~/.git-flow-completion.sh) and put the following line in
 #        your .bashrc:
-# 
+#
 #            source ~/.git-flow-completion.sh
-# 
+#
 #  2. If you are using Git < 1.7.1: Edit git-completion.sh and add the following line to the giant
 #     $command case in _git:
-# 
+#
 #         flow)        _git_flow ;;
-# 
-# 
+#
+#
 # The Fine Print
 # --------------
-# 
-# Copyright (c) 2010 [Justin Hileman](http://justinhileman.com)
-# 
+#
+# Copyright (c) 2011 [Justin Hileman](http://justinhileman.com)
+#
 # Distributed under the [MIT License](http://creativecommons.org/licenses/MIT/)
 
 _git_flow ()
 {
-	local subcommands="init feature release hotfix"
-	local subcommand="$(__git_find_subcommand "$subcommands")"
+	local subcommands="init feature release hotfix help version"
+	local subcommand="$(__git_find_on_cmdline "$subcommands")"
 	if [ -z "$subcommand" ]; then
 		__gitcomp "$subcommands"
 		return
 	fi
 
 	case "$subcommand" in
+	init)
+		__git_flow_init
+		return
+		;;
 	feature)
 		__git_flow_feature
 		return
@@ -73,10 +77,20 @@ _git_flow ()
 	esac
 }
 
+__git_flow_init ()
+{
+	local subcommands="help"
+	local subcommand="$(__git_find_on_cmdline "$subcommands")"
+	if [ -z "$subcommand" ]; then
+		__gitcomp "$subcommands"
+		return
+	fi
+}
+
 __git_flow_feature ()
 {
-	local subcommands="list start finish publish track diff rebase checkout pull"
-	local subcommand="$(__git_find_subcommand "$subcommands")"
+	local subcommands="list start finish publish track diff rebase checkout pull help"
+	local subcommand="$(__git_find_on_cmdline "$subcommands")"
 	if [ -z "$subcommand" ]; then
 		__gitcomp "$subcommands"
 		return
@@ -88,50 +102,43 @@ __git_flow_feature ()
 		return
 		;;
 	checkout|finish|diff|rebase)
-		__gitcomp "$(__git_flow_list_features)"
+		__gitcomp "$(__git_flow_list_branches 'feature')"
 		return
 		;;
 	publish)
-		__gitcomp "$(comm -23 <(__git_flow_list_features) <(__git_flow_list_remote_features))"
+		__gitcomp "$(comm -23 <(__git_flow_list_branches 'feature') <(__git_flow_list_remote_branches 'feature'))"
 		return
 		;;
 	track)
-		__gitcomp "$(__git_flow_list_remote_features)"
+		__gitcomp "$(comm -23 <(__git_flow_list_remote_branches 'feature') <(__git_flow_list_branches 'feature'))"
 		return
 		;;
 	*)
 		COMPREPLY=()
 		;;
 	esac
-}
-
-__git_flow_list_features ()
-{
-	git flow feature list 2> /dev/null | tr -d ' |*'
-}
-
-__git_flow_list_remote_features ()
-{
-	git branch -r 2> /dev/null | grep "origin/$(__git_flow_feature_prefix)" | awk '{ sub(/^origin\/$(__git_flow_feature_prefix)/, "", $1); print }'
-}
-
-__git_flow_feature_prefix ()
-{
-	git config gitflow.prefix.feature 2> /dev/null || echo "feature/"
 }
 
 __git_flow_release ()
 {
-	local subcommands="list start finish"
-	local subcommand="$(__git_find_subcommand "$subcommands")"
+	local subcommands="list start finish track publish help"
+	local subcommand="$(__git_find_on_cmdline "$subcommands")"
 	if [ -z "$subcommand" ]; then
 		__gitcomp "$subcommands"
 		return
 	fi
-	
+
 	case "$subcommand" in
 	finish)
-		__gitcomp "$(__git_flow_list_releases)"
+		__gitcomp "$(__git_flow_list_branches 'release')"
+		return
+		;;
+	publish)
+		__gitcomp "$(comm -23 <(__git_flow_list_branches 'release') <(__git_flow_list_remote_branches 'release'))"
+		return
+		;;
+	track)
+		__gitcomp "$(comm -23 <(__git_flow_list_remote_branches 'release') <(__git_flow_list_branches 'release'))"
 		return
 		;;
 	*)
@@ -139,17 +146,12 @@ __git_flow_release ()
 		;;
 	esac
 
-}
-
-__git_flow_list_releases ()
-{
-	git flow release list 2> /dev/null
 }
 
 __git_flow_hotfix ()
 {
-	local subcommands="list start finish"
-	local subcommand="$(__git_find_subcommand "$subcommands")"
+	local subcommands="list start finish help"
+	local subcommand="$(__git_find_on_cmdline "$subcommands")"
 	if [ -z "$subcommand" ]; then
 		__gitcomp "$subcommands"
 		return
@@ -157,7 +159,7 @@ __git_flow_hotfix ()
 
 	case "$subcommand" in
 	finish)
-		__gitcomp "$(__git_flow_list_hotfixes)"
+		__gitcomp "$(__git_flow_list_branches 'hotfix')"
 		return
 		;;
 	*)
@@ -166,12 +168,31 @@ __git_flow_hotfix ()
 	esac
 }
 
-__git_flow_list_hotfixes ()
+__git_flow_prefix ()
 {
-	git flow hotfix list 2> /dev/null
+	case "$1" in
+	feature|release|hotfix)
+		git config "gitflow.prefix.$1" 2> /dev/null || echo "$1/"
+		return
+		;;
+	esac
 }
 
-# temporarily wrap __git_find_on_cmdline() for backwards compatibility
-if [ -z "`type -t __git_find_subcommand`" ]; then
-	alias __git_find_subcommand=__git_find_on_cmdline
+__git_flow_list_branches ()
+{
+	local prefix="$(__git_flow_prefix $1)"
+	git branch 2> /dev/null | tr -d ' |*' | grep "^$prefix" | sed s,^$prefix,, | sort
+}
+
+__git_flow_list_remote_branches ()
+{
+	local prefix="$(__git_flow_prefix $1)"
+	local origin="$(git config gitflow.origin 2> /dev/null || echo "origin")"
+	git branch -r 2> /dev/null | sed "s/^ *//g" | grep "^$origin/$prefix" | sed s,^$origin/$prefix,, | sort
+}
+
+# alias __git_find_on_cmdline for backwards compatibility
+if [ -z "`type -t __git_find_on_cmdline`" ]; then
+	alias __git_find_on_cmdline=__git_find_subcommand
 fi
+
