2b28070c8c2bf1249f0ab942d10de840a0e5cf30
cange <info@psykmedia.de>
2012-07-04 23:44:14 +0200

    updated custome bash aliases

--- .bash_it/custom/internal.aliases.bash
+++ .bash_it/custom/internal.aliases.bash
@@ -1,15 +1,19 @@
-alias pb="cd ~/Documents/workspace/planningboard"
+# compass shotcuts
+alias cw='compass watch'
+alias cc='compass compile --force'
 
-# internal
-# alias jettys="cd ~/Documents/workspace/deposit-solutions/comonea/comonea-shop-webapp/ && MAVEN_OPTS="-XX:+CMSClassUnloadingEnabled -XX:PermSize=64M -XX:MaxPermSize=128M" mvn -Ddb.password= jetty:run"
-export COMONEA="${HOME}/Documents/workspace/deposit-solutions/comonea"
-alias com='cd $COMONEA'
-alias com-start='cd ${COMONEA}/comonea-shop-webapp/ && MAVEN_OPTS="-XX:+CMSClassUnloadingEnabled -XX:PermSize=64M -XX:MaxPermSize=128M" mvn -Ddb.username=comoneaowner jetty:run'
-alias com-rfq="cd $COMONEA/comonea-rfq-webapp/ && mvn -Ddb_rfq.username=comoneaowner jetty:run"
-alias com-pro="cd $COMONEA/comonea-shop-webapp/ && mvn jetty:run"
-# dkp
-alias dkp="cd ${COMONEA}/../dkp-web"
-alias ftm="cd ${COMONEA}/../dkp-cms/trunk/src/main"
-#
-# Xing pandora sandbox syncronisation
-alias rsync='rsync -v -zar -p -C --exclude=.git* --exclude=htdocs/ --exclude=conf/settings* --exclude=tmp/ --exclude=gems --exclude ".tm_sync.config" -e "ssh -p 2222" . "vagrant@sandbox:sandbox/rails-app"'
+
+if [$(uname) = "Linux"]; then
+  alias work='cd ~/Documents/workspace'
+  # projects
+  export COMONEA="$HOME/Documents/workspace/deposit-solutions/comonea"
+  alias com='cd $COMONEA'
+  alias com-start='cd ${COMONEA}/comonea-shop-webapp/ && MAVEN_OPTS="-XX:+CMSClassUnloadingEnabled -XX:PermSize=64M -XX:MaxPermSize=128M" mvn -Ddb.username=comoneaowner jetty:run'
+  alias com-rfq="cd $COMONEA/comonea-rfq-webapp/ && mvn -Ddb_rfq.username=comoneaowner jetty:run"
+  alias com-pro="cd $COMONEA/comonea-shop-webapp/ && mvn jetty:run"
+  alias dkp="cd $COMONEA/../dkp-web"
+  alias ftm="cd $COMONEA/../dkp-cms/trunk/src/main"
+else
+  # mac os specifc
+  alias work='cd ~/workspace'
+fi
