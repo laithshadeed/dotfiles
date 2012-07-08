ec9b1df874e59115bca72652d17f240acf737d6f
cange <info@psykmedia.de>
2012-07-08 22:49:03 +0200

    fixed broken if syntax for bash condition

--- .bash_it/custom/internal.aliases.bash
+++ .bash_it/custom/internal.aliases.bash
@@ -3,7 +3,7 @@ alias cw='compass watch'
 alias cc='compass compile --force'
 
 
-if [$(uname) = "Linux"]; then
+if [ $(uname) = "Linux" ]; then
   alias work='cd ~/Documents/workspace'
   # projects
   export COMONEA="$HOME/Documents/workspace/deposit-solutions/comonea"
