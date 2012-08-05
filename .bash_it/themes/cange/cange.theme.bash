48b466ca1af4c5eec8c81b14cbe2021475aa26d6
Christian Angermann <christian.angermann@xing.com>
2012-08-05 12:21:56 +0200

    changed promp symbols #bash

--- .bash_it/themes/cange/cange.theme.bash
+++ .bash_it/themes/cange/cange.theme.bash
@@ -4,17 +4,17 @@
 
 git_prompt_status() {
   local git_status_output
-
+  prefix="${normal}⎇  "
   git_status_output=$(git status 2> /dev/null )
 
   if [ -n "$(echo $git_status_output | grep 'Changes not staged')" ]; then
-    git_status="${red}$(git_prompt_info) ✖ "
+    git_status="$prefix${red}$(git_prompt_info) ✖ "
   elif [ -n "$(echo $git_status_output | grep 'Changes to be committed')" ]; then
-    git_status="${yellow}$(git_prompt_info)${bold_yellow} ^ "
+    git_status="$prefix${yellow}$(git_prompt_info)${bold_yellow} ⌅ "
   elif [ -n "$(echo $git_status_output | grep 'Untracked files')" ]; then
-    git_status="${orange}$(git_prompt_info)${bold_orange} + "
+    git_status="$prefix${orange}$(git_prompt_info) ✚ "
   elif [ -n "$(echo $git_status_output | grep 'nothing to commit')" ]; then
-    git_status="${green}$(git_prompt_info) ✔ "
+    git_status="$prefix${green}$(git_prompt_info) ✔ "
   else
     git_status="$(git_prompt_info)"
   fi
@@ -22,10 +22,11 @@ git_prompt_status() {
 }
 
 
+
 # for example:
 # + branch-name directory-name $
 function prompt_command() {
-  PS1="$(git_prompt_status)${cyan}\W${normal} \$ "
+  PS1="$(git_prompt_status)${cyan}\W${normal} ➜ "
 }
 PROMPT_COMMAND=prompt_command;
 ## git-theme
