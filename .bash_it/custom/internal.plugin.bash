2679855135ea1ad011c0e051e3ccde61bb951c45
Christian Angermann <christian.angermann@xing.com>
2012-10-19 20:04:02 +0200

    added bash history by search of starting chars

--- .bash_it/custom/internal.plugin.bash
+++ .bash_it/custom/internal.plugin.bash
@@ -3,4 +3,20 @@ push_this_to_remote() {
   current_branch=`git branch | grep "*" | awk '{print $2}'`
   git push xws-features $current_branch
   git branch --set-upstream $current_branch xws-features/$current_branch
-}
+  }
+
+#### history begin ####
+bind '"\e[A":history-search-backward'
+bind '"\e[B":history-search-forward'
+# don't put duplicate lines in the history. See bash(1) for more options
+# don't overwrite GNU Midnight Commander's setting of `ignorespace'.
+export HISTCONTROL=$HISTCONTROL${HISTCONTROL+,}ignoredups
+# ... or force ignoredups and ignorespace
+export HISTCONTROL=ignoreboth
+# append to the history file, don't overwrite it
+shopt -s histappend
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+export HISTSIZE=10000
+export HISTCONTROL=erasedups
+shopt -s histappend
+#### history end   ####
