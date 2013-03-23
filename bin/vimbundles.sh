cf48f67ce1a65294867a149702afd6c7b2ad6433
Christian Angermann <christian.angermann@xing.com>
2013-03-23 10:22:06 +0100

    Add basic JSHint file for coding conventions

--- bin/vimbundles.sh
+++ bin/vimbundles.sh
@@ -38,8 +38,8 @@ get_bundle kien           ctrlp.vim             # quick file finder in directory
 get_bundle Raimondi       delimitMate           # provides automatic closing of quotes, parenthesis, brackets, etc.
 get_bundle vim-scripts    Gundo                 # file history
 get_bundle claco          jasmine.vim           # JavaScript testing
-get_bundle vim-scripts    jshint.vim            # JavaScript syntax validation
-get_bundle hallettj       jslint.vim            # JSLint is a handy tool that spots errors and common mistakes in JavaScript code
+get_bundle walm           jshint.vim            # JavaScript syntax validation
+# get_bundle hallettj       jslint.vim            # JSLint is a handy tool that spots errors and common mistakes in JavaScript code
 get_bundle vim-scripts    L9                    # l9 is a Vim-script library, which provides some utility functions and commands for programming in Vim.
 get_bundle vim-scripts    matchit.zip
 get_bundle scrooloose     nerdtree              # The NERD tree allows you to explore your filesystem and to open files and directories
