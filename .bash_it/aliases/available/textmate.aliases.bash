5a5ee21cbab23032f13b0cf71597870b535c1e5d
Christian Angermann <christian.angermann@nb-cangermann.local>
2013-03-23 22:55:04 +0100

    Exclude .bash_it to better up to date the dot files

--- .bash_it/aliases/available/textmate.aliases.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-case $OSTYPE in
-  darwin*)
-    # Textmate
-    alias e='mate . &'
-    alias et='mate app config db lib public script test spec config.ru Gemfile Rakefile README &'
-    ;;
-esac
