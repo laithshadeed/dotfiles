22ca371a6b7ddb0a015019d508b7811f3b9bdb63
cange <psykmedia@gmail.com>
2013-06-09 16:16:08 +0200

    Update internal aliases

--- oh-my-zsh/custom/internal.aliases.zsh
+++ oh-my-zsh/custom/internal.aliases.zsh
@@ -16,22 +16,27 @@ alias rm-orig='find . -type f -name "*.orig" -exec rm -f {} \;'
 if [ $(uname) = "Linux" ]; then
   alias work='cd ~/Documents/workspace'
   # projects
-  export COMONEA="$HOME/Documents/workspace/deposit-solutions/ds-comonea"
-  export COMONEA_PRO="$HOME/Documents/workspace/deposit-solutions/ds-comonea-pro"
-  alias com="cd $COMONEA_PRO"
-  alias com-start='cd ${COMONEA}/comonea-shop-webapp/ && MAVEN_OPTS="-XX:+CMSClassUnloadingEnabled -XX:PermSize=64M -XX:MaxPermSize=128M" mvn -Ddb.username=comoneaowner jetty:run'
-  alias com-ovl="cd $COMONEA/comonea-core/comonea-overlayed-webapp/"
-  alias com-b2c="cd $COMONEA/comonea-b2c/comonea-b2c-shop-webapp/ && mvn jetty:run"
+  DEPOSIT_SOLUTIONS="~/Documents/workspace/deposit-solutions"
+  COMONEA_B2C="$DEPOSIT_SOLUTIONS/ds-comonea-b2c"
+  COMONEA_PRO="$DEPOSIT_SOLUTIONS/ds-comonea-pro"
+  alias com_b2c="cd $COMONEA_B2C"
+  alias com_pro="cd $COMONEA_PRO"
+  # alias com-start='cd ${COMONEA}/comonea-shop-webapp/ && MAVEN_OPTS="-XX:+CMSClassUnloadingEnabled -XX:PermSize=64M -XX:MaxPermSize=128M" mvn -Ddb.username=comoneaowner jetty:run'
+  alias com-b2c="cd $COMONEA_B2C/comonea-b2c-shop-webapp/ && mvn jetty:run"
   alias com-pro="cd $COMONEA_PRO/cp-customer-webapp && mvn -Ddb.username=comonea_pro_o jetty:run"
   alias cp-cli="java -Ddb.username=comonea_pro_o -jar $COMONEA_PRO/cp-cli/target/cp-cli-*-jar-with-dependencies.jar"
-  alias ds='cd $COMONEA/../ds-websites/'
-  alias dkp='cd $COMONEA/../ds-websites/dkp-web'
-  alias ftm='cd $COMONEA/../dkp-cms/trunk/src/main'
+  alias ds="cd $DEPOSIT_SOLUTIONS/ds-websites/"
+  alias dkp="cd $DEPOSIT_SOLUTIONS/ds-websites/dkp-web"
+  # system
   alias vim='gvim'
   alias gitx='gitg'
 else
   # mac os specifc
   alias work='cd ~/workspace'
+  # system
+  alias vim='mvim'
+  alias gitg='gitx'
+  # xws environment
   alias cdm='cd ~/workspace/xws'
   alias cdi='cd ~/workspace/xws/public/img/xing/xe/web_service'
   alias xws='cd ~/workspace/xws/engines/web_service'
@@ -39,9 +44,6 @@ else
   alias xync='xws && xing sandbox sync'
   alias rwst='bundle exec rake web_service:test:all'
   alias rjst='phantomjs ~/workspace/xws/public/js/xe/web_service/test/qunit/addons/phantomjs/runner.js '
-  alias vim='mvim'
-  alias gitg='gitx'
-  # xws environment
   alias ccp='xws && compass compile --force -e production -s compressed'
   alias cc='xws && compass compile --force'
   alias cw='xws && compass watch --time --trace'
