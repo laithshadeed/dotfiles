701d59ee43cd549ce6c012192dc561c3e5188521
Christian Angermann <christian.angermann@wlw.de>
2015-05-22 15:29:28 +0200

    Clean up aliases

--- oh-my-zsh/custom/internal.aliases.zsh
+++ oh-my-zsh/custom/internal.aliases.zsh
@@ -3,7 +3,7 @@
 alias q='exit'
 alias k='clear'
 
-alias sgrun='cd ~/workspace/wlw_styleguide/spec/dummy/ && rails s -p 3002'
+alias sgrun='cd ~/workspace/wlw/wlw_styleguide/spec/dummy/ && rails s -p 3002'
 # bundle
 alias be='bundle exec'
 alias bi='bundle install'
@@ -11,22 +11,12 @@ alias rwst="bundle exec rake web_service:test:all"
 alias server="sudo python -m SimpleHTTPServer 80"
 
 alias work='cd ~/workspace'
-alias wlw='cd ~/workspace/wlw'
-alias sg='cd ~/workspace/wlw_styleguide'
+alias wlw='cd ~/workspace/wlw/wlw'
+alias sg='cd ~/workspace/wlw/wlw_styleguide'
+alias cr='cd ~/workspace/wlw/customer_report'
+alias ac='cd ~/workspace/wlw/accounts'
 
 if [ $(uname) = "Linux" ]; then
-  # projects
-  DEPOSIT_SOLUTIONS="~/Documents/workspace/deposit-solutions"
-  COMONEA_B2C="$DEPOSIT_SOLUTIONS/ds-comonea-b2c"
-  COMONEA_PRO="$DEPOSIT_SOLUTIONS/ds-comonea-pro"
-  alias com_b2c="cd $COMONEA_B2C"
-  alias com_pro="cd $COMONEA_PRO"
-  # alias com-start='cd ${COMONEA}/comonea-shop-webapp/ && MAVEN_OPTS="-XX:+CMSClassUnloadingEnabled -XX:PermSize=64M -XX:MaxPermSize=128M" mvn -Ddb.username=comoneaowner jetty:run'
-  alias com-b2c="cd $COMONEA_B2C/comonea-b2c-shop-webapp/ && mvn jetty:run"
-  alias com-pro="cd $COMONEA_PRO/cp-customer-webapp && mvn -Ddb.username=comonea_pro_o jetty:run"
-  alias cp-cli="java -Ddb.username=comonea_pro_o -jar $COMONEA_PRO/cp-cli/target/cp-cli-*-jar-with-dependencies.jar"
-  alias ds="cd $DEPOSIT_SOLUTIONS/ds-websites/"
-  alias dkp="cd $DEPOSIT_SOLUTIONS/ds-websites/dkp-web"
   # system
   alias vim='gvim'
   alias gitx='gitg'
@@ -36,6 +26,5 @@ else
   alias gitg='gitx'
   # xws environment
   alias cdm='cd ~/workspace/xws'
-  alias xync='cdm && xing sandbox sync'
   alias rwst='bundle exec rake web_service:test:all'
 fi
