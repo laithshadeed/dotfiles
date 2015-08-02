597c2a802f79a3c0658e587189adb9ed71cf9c97
Christian Angermann <christian.angermann@wlw.de>
2015-08-02 13:35:55 +0200

    Avoid commdef warring for oh-my-zsh

--- oh-my-zsh/custom/git.plugin.zsh
+++ oh-my-zsh/custom/git.plugin.zsh
@@ -1,6 +1,6 @@
 alias gm='git merge'
-compdef _git gm=git-merge
+# compdef _git gm=git-merge
 alias gsh='git stash'
-compdef _git gsh=git-stash
+# compdef _git gsh=git-stash
 alias gsp='git stash pop'
-compdef _git gsp=git-stash-pop
+# compdef _git gsp=git-stash-pop
